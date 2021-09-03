8e6ab263cdb4d84c716203b55dd61763672d4ea2
Misael Adame <misael_adame@protonmail.com>
2021-09-03 12:08:48 -0500

    Tipos, Operadores, Variables

---
+++ 01_Tipos-Operadores-Variables.py
@@ -0,0 +1,26 @@
+
+print(5 + 6)
+print(10 % 3)
+print(5 ** 3)
+print(9 // 2)
+
+nombre = 5
+print(type(nombre))
+
+nombre = 5.2
+print(type(nombre))
+
+mensaje = """Esto es un mensaje 
+    con tres saltos 
+    de linea"""
+
+print(mensaje)
+
+numero1 = 5
+numero2 = 7
+if numero1 > numero2:
+    print("El numero 1 es mayor")
+else:
+    print("El numero 2 es mayor")
+
+
