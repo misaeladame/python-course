b8c35b8bab024cc3b3332de0ec0aa4e8b0c177e4
Misael Adame <misael_adame@protonmail.com>
2021-09-10 11:11:22 -0500

    Funciones con parametros

---
+++ 03_Funciones con parametros.py
@@ -0,0 +1,20 @@
+def suma(num1, num2):
+    print(num1 + num2)
+
+suma(5, 7)
+
+suma(2, 3)
+
+suma(35, 358)
+
+
+def sumar(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+print(sumar(3, 4))
+print(sumar(9, 15))
+print(sumar(24, 58))
+
+almacena_resultado = sumar(5, 8)
+print(almacena_resultado)
