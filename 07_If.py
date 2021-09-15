188dd3d077120cfe52f1558a99ec49569d9abcd8
Misael Adame <misael_adame@protonmail.com>
2021-09-14 20:28:16 -0500

    Condicional If

---
+++ 07_If.py
@@ -0,0 +1,14 @@
+print("Programa de evaluación de notas de alumnos")
+
+nota_alumno = input("Introduce la nota del alumno: ")
+#nota_alumno = int(input("Introduce la nota del alumno: "))
+
+def evaluacion(nota):
+    valoracion = "aprobado"
+    if nota < 5:
+        valoracion = "reprobado"
+    return valoracion
+
+
+print(evaluacion(int(nota_alumno)))
+#print(evaluacion(nota_alumno))
