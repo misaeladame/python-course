d3635de794335f98277a3f1b4e3274a1d3d41a19
Misael Adame <misael_adame@protonmail.com>
2021-09-26 15:12:47 -0500

    Continue, pass y else en bucles

---
+++ 17_Continue - pass - else.py
@@ -0,0 +1,37 @@
+for letra in "Python":
+    if letra == "h":
+        continue
+    print("Viendo la letra: " +letra)
+
+print()
+#------------------------------------------------------------------
+nombre = "Pildoras informaticas"
+
+contador=0
+
+for i in nombre:
+    if i == " ":
+        continue
+    contador+=1
+
+print(contador)
+
+print()
+"""
+#-------------------------------------------------------------------
+while True:
+    pass # devuelve un null
+#-------------------------------------------------------------------
+class MiClase:
+    pass # Para implementar mas tarde
+#-------------------------------------------------------------------
+"""
+email=input("Introduce tu email, por favor: ")
+for i in email:
+    if i == "@":
+        arroba = True
+        break;
+else:
+    arroba = False
+
+print(arroba)
