d6c836496a3168ed053df9577c353e0ec303e84e
Misael Adame <misael_adame@protonmail.com>
2021-09-26 14:23:41 -0500

    Lo basico de un While

---
+++ 15_While.py
@@ -0,0 +1,17 @@
+#---------------------------------------------------------
+i = 1
+
+while i<=10:
+    print("Ejecución " +str(i))
+    i=i+1
+
+print("Terminó la ejecución")
+#---------------------------------------------------------
+edad = int(input("Introduce tu edad por favor: "))
+
+while edad < 5 or edad > 100:
+    print("Has introducido una edad incorrecta. Vuelve a intentarlo")
+    edad = int(input("Introduce tu edad por favor: "))
+
+print("Gracias por colaborar. Puedes pasar")
+print("Edad del aspirante " +str(edad))
