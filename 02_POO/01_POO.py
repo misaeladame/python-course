e0fe65465f84cc3e7ce0e9e1917cbe7ae6fbc294
Misael Adame <misael_adame@protonmail.com>
2022-04-02 18:22:43 -0600

    Contructores y Encapsulamiento

--- 02_POO/01_POO.py
+++ 02_POO/01_POO.py
@@ -1,23 +1,38 @@
 class Coche():
-  # 4 propiedades
-  largoChasis = 250 
-  anchoChasis = 120
-  ruedas = 4
-  enmarcha = False
+
+  def __init__(self):
+    # 4 propiedades
+    # self = this
+    # __ = private
+
+    self.__largoChasis = 250 
+    self.__anchoChasis = 120
+    self.__ruedas = 4
+    self.__enmarcha = False
 
   # 2 comportamientos
-  def arrancar(self):
-    self.enmarcha = True
+  def arrancar(self,arrancamos):
+    self.__enmarcha = arrancamos
 
-  def estado(self):
-    if(self.enmarcha):
+    if(self.__enmarcha):
       return "El coche está en marcha"
     else:
       return "El coche está parado"
 
+  def estado(self):
+    print("El coche tiene ", self.__ruedas, "ruedas. Un ancho de", self.__anchoChasis, "y un largo de", self.__largoChasis)
+
 miCoche = Coche()  # Instanciar una clase
 
-print("El largo del coche es:", miCoche.largoChasis)
-print("El coche tiene", miCoche.ruedas, "ruedas")
-miCoche.arrancar()
-print(miCoche.estado())
+# print("El largo del coche es:", miCoche.largoChasis)
+# print("El coche tiene", miCoche.ruedas, "ruedas")
+print(miCoche.arrancar(True))
+miCoche.estado()
+
+print("------------- A continuación creamos el segundo objeto -----------------")
+
+miCoche2 = Coche()
+
+print(miCoche2.arrancar(False))
+
+miCoche2.estado()
