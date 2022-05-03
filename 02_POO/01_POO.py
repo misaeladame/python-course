6b2a9569d162dbdda712c85650dc13f6284c3a1e
Misael Adame <misael_adame@protonmail.com>
2022-05-02 22:38:08 -0500

    Más POO

--- 02_POO/01_POO.py
+++ 02_POO/01_POO.py
@@ -1,5 +1,6 @@
 class Coche():
 
+  # Constructor
   def __init__(self):
     # 4 propiedades
     # self = this
@@ -15,12 +16,31 @@ class Coche():
     self.__enmarcha = arrancamos
 
     if(self.__enmarcha):
+      chequeo = self.__chequeo_interno()
+
+
+    if(self.__enmarcha and chequeo):
       return "El coche está en marcha"
+    elif(self.__enmarcha and chequeo==False):
+      return "Algo ha ido mal en el chequeo, no podemos arrancar"
     else:
       return "El coche está parado"
 
   def estado(self):
     print("El coche tiene ", self.__ruedas, "ruedas. Un ancho de", self.__anchoChasis, "y un largo de", self.__largoChasis)
+
+  def __chequeo_interno(self):
+    print("Realizando chequeo interno")
+
+    self.gasolina = "ok"
+    # self.aceite = "ok"
+    self.aceite = "mal"
+    self.puertas = "cerradas"
+
+    if (self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas"):
+      return True
+    else:
+      return False  
 
 miCoche = Coche()  # Instanciar una clase
 
@@ -29,6 +49,8 @@ miCoche = Coche()  # Instanciar una clase
 print(miCoche.arrancar(True))
 miCoche.estado()
 
+# print(miCoche.chequeo_interno())
+
 print("------------- A continuación creamos el segundo objeto -----------------")
 
 miCoche2 = Coche()
@@ -36,3 +58,5 @@ miCoche2 = Coche()
 print(miCoche2.arrancar(False))
 
 miCoche2.estado()
+
+# print(miCoche2.chequeo_interno())
