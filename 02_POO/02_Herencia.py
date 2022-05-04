38f40a4f0cf34ec9c67c5aa2f385dd5f6a2f379d
Misael Adame <misael_adame@protonmail.com>
2022-05-04 13:45:56 -0500

    Herencia múltiple

--- 02_POO/02_Herencia.py
+++ 02_POO/02_Herencia.py
@@ -19,9 +19,46 @@ class Vehiculos():
   def estado(self):
     print("Marca:", self.marca, "\nModelo:", self.modelo, "\nEn Marcha:", self.enmarcha, "\nAcelerando:", self.acelera, "\nFrenando:" ,self.frena)
 
+class Furgoneta(Vehiculos):
+  
+  def carga(self, cargar):
+    self.cargado = cargar
+    if(self.cargado):
+      return "La furgoneta está cargada"
+    else:
+      return "La furgoneta no está cargada" 
+
 class Moto(Vehiculos):
-  pass
+  hcaballito = ""
+
+  def caballito(self):
+    self.hcaballito = "Voy haciendo el caballito"
+
+  def estado(self):
+    print("Marca:", self.marca, "\nModelo:", self.modelo, "\nEn Marcha:", self.enmarcha, "\nAcelerando:", self.acelera, "\nFrenando:" ,self.frena, "\n", self.hcaballito)
+
+class VElectricos():
+  
+  def __init__(self):
+    self.autonomia = 100
+
+  def cargarEnergia(self):
+    self.cargando = True
 
 miMoto = Moto("Honda", "CBR")
 
+miMoto.caballito()
+
 miMoto.estado()
+
+miFurgoneta = Furgoneta("Renault", "Kangoo")
+
+miFurgoneta.arrancar()
+miFurgoneta.estado()
+print(miFurgoneta.carga(True))
+
+class BicicletaElectrica(VElectricos, Vehiculos):
+  pass
+
+miBici = BicicletaElectrica()
+
